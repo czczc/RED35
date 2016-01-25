2410e96fb7afebb686db062fce6d95e871f78b5a
jdjonathandavies <jdjonathandavies@googlemail.com>
2016-01-25 15:24:16 +0000

    Jonathan Davies - Adding optional file name supply to the run2d.C macro. Also check that the file exists before opening

--- scripts/run2d.C
+++ scripts/run2d.C
@@ -1,4 +1,16 @@
+void run2d(const char* fileName=0)
 {
+  if(fileName){
+    std::cout << "RED35 - Opening fileName \"" << fileName << "\"" << std::endl;
+    bool file_does_not_exist = gSystem->AccessPathName(fileName); //This function returns FALSE if the file exists!
+    if(file_does_not_exist){
+      std::cout << "RED35 - File does not exist!" << std::endl;
+      return;
+    }
+  }
+  else{
+    std::cout << "RED35 - You didn't supply a filename ( root -l run2d.C\(\"path/to/file.root\"\) )" << std::endl;
+  }
     gROOT->Reset();
     gROOT->ProcessLine(".x loadClasses.C" );
 
@@ -6,5 +18,5 @@
     //   const char* filename=0,
     //   int nTDCTicks=3200,
     //   double xPerTDC=0.0802815);
-    GuiController *gc = new GuiController(gClient->GetRoot(), 1200, 900, 0, 3200, 0.0802815);
+    GuiController *gc = new GuiController(gClient->GetRoot(), 1200, 900, fileName, 3200, 0.0802815);
 }
